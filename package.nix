32488c97ff766ecfa2bb2b171284c9ef527ccc29
Andrew Baxter <>
2026-07-20 18:03:02 +0900

    Bump fenix

--- package.nix
+++ package.nix
@@ -1,6 +1,6 @@
 { pkgs, lib }:
 let
-  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/1a79901b0e37ca189944e24d9601c8426675de50.zip") { };
+  fenix = import (fetchTarball "https://github.com/nix-community/fenix/archive/480c95c3c9ca478e857f7153639010fb54ec73e8.zip") { };
   naersk = pkgs.callPackage (fetchTarball "https://github.com/nix-community/naersk/archive/378614f37a6bee5a3f2ef4f825a73d948d3ae921.zip") (
     let
       toolchain = fenix.combine [
